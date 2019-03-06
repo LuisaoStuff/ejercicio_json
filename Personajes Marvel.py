807c7033022a24faa98d65e20972bbe6134ac007
Luis Vazquez Alejo <luisvazquezalejo@gmail.com>
2019-03-06 14:15:24 +0100

    Arreglo y normalizo parte del json, ademas de terminar el ejercicio 2 y empezar el 3

--- Personajes Marvel.py
+++ Personajes Marvel.py
@@ -9,6 +9,19 @@ from os import system
 #						      Funciones							       #
 ########################################################################
 
+def EliminarRepetidos(lista):	#	Recibe una lista y elimina los elementos repetidos.
+
+	ListaSinRepetir = []
+	for A in lista:
+		if A not in ListaSinRepetir:
+			ListaSinRepetir.append(A)
+	return sorted(ListaSinRepetir)	#	Devuelve una lista de elementos únicos ordenada.
+
+def LimpiarCadena(Cadena):	#	Esta función elimina algunos caracteres de una cadena. La uso sobre todo
+							#	dentro de la función BuscadorDeJuegos(juego)
+	CadenaLimpia=Cadena.upper().replace("-","").replace(" ","").replace("(","").replace(")","").replace(".","").strip()
+	return CadenaLimpia
+
 def clear(Espaciado):	#	Pequeña funcion que simula un clear, además recibe un entero para centrar
 						#	el texto que lo sigue a continuación.
 	system('clear')
@@ -38,6 +51,8 @@ def fehInstalado():
 				Miniaturas=False
 		else:
 			Miniaturas=True
+	Salida.close()
+	system('rm salida.txt')
 
 def Desinstalar_feh():
 
@@ -49,6 +64,39 @@ def Desinstalar_feh():
 			Eleccion=input('	   Antes de salir, ¿desea desinstalar el paquete "feh"?	').upper()
 			if Eleccion in Afirmacion:
 				system('sudo apt-get remove -y feh')
+	Salida.close()
+	system('rm salida.txt')
+
+def ListaPersonajes(Diccionario):
+	Lista=[]
+	for Personaje in Diccionario["characters"]:
+		Lista.append(Personaje)
+
+	return sorted(Lista)
+
+def BuscarPersonaje(Busqueda,Lista1):
+	for Personaje in Lista1:
+		Coincidencia=LimpiarCadena(Personaje)
+		if Coincidencia==Busqueda:
+			
+			return Personaje
+
+
+def ContadorApariciones(Personaje,Diccionario):
+
+	Total=Diccionario["characters"][BuscarPersonaje(Personaje,ListaPersonajes(Diccionario))]["comics"]["available"]
+	return Total
+
+def Categorias(Diccionario):
+
+	Categorias=[]
+	for Personaje in Diccionario["characters"]:
+		
+		for categoria in Diccionario["characters"][Personaje]["wiki"]["categories"]:
+			print(categoria)
+			Categorias.append(categoria)
+
+	return EliminarRepetidos(Categorias)
 
 
 ########################################################################
@@ -61,18 +109,14 @@ with open("Personajes Marvel.json","r") as fichero:
 
 	fehInstalado()
 
-	Pausa()
-
-	while True:
-
-																############################
+	while True:													############################
 																#			Menú           #
 		clear(0)												############################
 		print('''\n\n	Elige una de las siguientes opciones:				
 
 		1. Lista los nombres de todos los personajes
 		2. Contador de apariciones en comics
-		3. 
+		3. Personajes por categoría
 		4. 
 		5. 
 		0. Salir
@@ -91,15 +135,36 @@ with open("Personajes Marvel.json","r") as fichero:
 			if opcion==0:		#############
 								#	Salir   #
 				clear(0)		#############
-				Desinstalar_feh()
+			#s	Desinstalar_feh()
 				clear(0)
 				break
-'''
+
 			elif opcion==1:
-
-			elif opcion==2:											
-
+				clear(0)
+				Lista=ListaPersonajes(Diccionario)
+				for Personaje in Lista:
+					print("			",Personaje)
+				Pausa()
+	
+			elif opcion==2:
+				clear(9)
+				Busqueda=LimpiarCadena(input('''			    Introduce un personaje
+		  	    > '''))
+				Total=ContadorApariciones(Busqueda,Diccionario)
+				print("		",BuscarPersonaje(Busqueda,ListaPersonajes(Diccionario)))
+				print("		Apariciones >",Total,"\n")
+				Pausa()
+	
 			elif opcion==3:
+				
+				clear(9)			
+				for categoria in Categorias(Diccionario):
+					print(categoria)
+				Busqueda=LimpiarCadena(input('''			    Introduce un personaje
+		  	    > '''))
+
+'''										
+
 
 			elif opcion==4:
 
