327fee6babe5ca065ffb1726563d19a068465ec2
Luis Vazquez Alejo <luisvazquezalejo@gmail.com>
2019-03-06 20:01:34 +0100

    Termino de comentar el código

--- Personajes Marvel.py
+++ Personajes Marvel.py
@@ -32,11 +32,11 @@ def Pausa():
 	input('\n		  "Pusa enter" para volver al menú...')
 
 def fehInstalado():
-
-	system('whereis feh > salida.txt')
-	clear(7)
-	with open("salida.txt","r") as Salida:
-		if Salida.readlines()[0]=='feh:\n':
+													#######################################################
+	system('whereis feh > salida.txt')				#	Comprueba  si  tienes instalado  el paquete feh   #
+	clear(7)										#	para luego poder ejecutar el  comando. Si no lo   #
+	with open("salida.txt","r") as Salida:			#	tienes instalado, te da la opcion de instalarlo   #
+		if Salida.readlines()[0]=='feh:\n':			#######################################################
 			print('''		Parece que no tienes instalado el paquete "feh"
 		Si desea que durante la ejecución de este python
 		se puedan mostrar las imágenes de los personajes
@@ -57,10 +57,10 @@ def fehInstalado():
 
 def Desinstalar_feh():
 
-	system('whereis feh > salida.txt')
-	clear(10)
-	with open("salida.txt","r") as Salida:
-		if Salida.readlines()[0]!='feh:\n':
+	system('whereis feh > salida.txt')					#############################################
+	clear(10)											#	Al final del programa te da la opcion   #
+	with open("salida.txt","r") as Salida:				#	de desinstalar el paquete feh 			#
+		if Salida.readlines()[0]!='feh:\n':				#############################################
 			Afirmacion=['YES','Y','SI','S']
 			Eleccion=input('	   Antes de salir, ¿desea desinstalar el paquete "feh"?	').upper()
 			if Eleccion in Afirmacion:
@@ -78,8 +78,8 @@ def ListaPersonajes(Diccionario):
 def BuscarPersonaje(Busqueda,Lista1):
 	try:
 		for Personaje in Lista1:
-			Coincidencia=LimpiarCadena(Personaje)
-			if Coincidencia==Busqueda:
+			Coincidencia=LimpiarCadena(Personaje)	#	Usa la funcion LimpiarCadena para "estandarizarla"
+			if Coincidencia==Busqueda:				#	y que de esta forma la busqueda sea menos estricta
 				return Personaje
 	except:
 		return 0
@@ -88,7 +88,7 @@ def ContadorApariciones(Personaje,Diccionario):
 	try:
 		Total=Diccionario["characters"][BuscarPersonaje(Personaje,ListaPersonajes(Diccionario))]["comics"]["available"]
 	except:
-		Total='No aparece'
+		Total='No aparece'				#	Simplemente cuenta los comics que tiene un personaje concreto
 	return Total
 
 def Categorias(Diccionario):
@@ -102,10 +102,10 @@ def Categorias(Diccionario):
 
 
 def PersonajesPorCategoria(Diccionario,Categoria):
-	Lista=[]
-	CategoriaLimpia=LimpiarCadena(Categoria)
-	for personaje in Diccionario["characters"]:
-
+	Lista=[]												#####################################################
+	CategoriaLimpia=LimpiarCadena(Categoria)				#	Devuelve una lista ordenada de los personajes   #
+	for personaje in Diccionario["characters"]:				#	dentro de una categoría concreta				#
+															#####################################################
 		for cat in Diccionario["characters"][personaje]["wiki"]["categories"]:
 			Coincidencia=LimpiarCadena(cat)
 			if Coincidencia==CategoriaLimpia:		
@@ -120,9 +120,9 @@ def ListaEventos(Diccionario):
 			Lista.append(evento["name"])
 	return EliminarRepetidos(Lista)
 
-def PersonajesPorEvento(Diccionario,evento):
-	Lista=[]
-	for personaje in Diccionario["characters"]:
+def PersonajesPorEvento(Diccionario,evento):				#	Busca por el diccionario los personajes que tienen
+	Lista=[]												#	un evento concreto dentro de events, y los mete en
+	for personaje in Diccionario["characters"]:				#	una lista.
 		if Diccionario["characters"][personaje]["events"]["available"]!=0:
 			for event in Diccionario["characters"][personaje]["events"]["items"]:
 				EventoL=LimpiarCadena(evento)
@@ -132,15 +132,15 @@ def PersonajesPorEvento(Diccionario,evento):
 	return Lista		
 
 def Miniatura(Personaje,Diccionario):
-	url=Diccionario["characters"][Personaje]["thumbnail"]["path"]
-	extension=Diccionario["characters"][Personaje]["thumbnail"]["extension"]
+	url=Diccionario["characters"][Personaje]["thumbnail"]["path"]				# Concatena los campos necesarios para
+	extension=Diccionario["characters"][Personaje]["thumbnail"]["extension"]	# devolver una url de la miniatura
 	urlcompleta=url+'.'+extension
 	return urlcompleta
 
-def EventosP(Personaje,Diccionario):
-	EncuentrosTotales=[]
-	ListaDeEventos=ListaEventos(Diccionario)
-	for evento in ListaDeEventos:
+def EventosP(Personaje,Diccionario):			#	Recibe un personaje. Lee todos los eventos en los que ha estado
+	EncuentrosTotales=[]						#	y los introduce en la funcion personajes por evento para crear
+	ListaDeEventos=ListaEventos(Diccionario)	#	una lista de eventos y personajes
+	for evento in ListaDeEventos:				#######################################################################
 		for eventoPersonaje in Diccionario["characters"][Personaje]["events"]["items"]:
 			if evento==eventoPersonaje["name"]:
 				Encuentro=[]
@@ -165,7 +165,7 @@ with open("Personajes Marvel.json","r") as fichero:
 
 	Diccionario = json.load(fichero)
 
-	Miniaturas=fehInstalado()
+	Miniaturas=fehInstalado()	#	Comprueba que "feh" está instalado
 
 
 	while True:													############################
@@ -194,71 +194,72 @@ with open("Personajes Marvel.json","r") as fichero:
 			if opcion==0:		#############
 								#	Salir   #
 				clear(0)		#############
-			#s	Desinstalar_feh()
+				Desinstalar_feh()	#<<<<<<<<<<<<<<< Da la opción de desinstalar "feh"
 				clear(0)
 				break
 
 			elif opcion==1:
-				clear(0)
-				Lista=ListaPersonajes(Diccionario)
-				for Personaje in Lista:
+				clear(0)							#################################################
+				Lista=ListaPersonajes(Diccionario)	#	Lista los nombres de todos los personajes   #
+				for Personaje in Lista:				#################################################
 					print("			",Personaje)
 				Pausa()
 	
-			elif opcion==2:
-				clear(9)
+			elif opcion==2:				#	Cuenta el número de comics que tiene el personaje introducido
+				clear(9)				#####################################################################
 				Busqueda=LimpiarCadena(input('''			    Introduce un personaje
 		  	    > '''))
-				Total=ContadorApariciones(Busqueda,Diccionario)
-				print("		",BuscarPersonaje(Busqueda,ListaPersonajes(Diccionario)))
-				print("		Apariciones >",Total,"\n")
-				Pausa()
-	
-			elif opcion==3:
-				clear(1)		
-				Cat=Categorias(Diccionario)	
+				Total=ContadorApariciones(Busqueda,Diccionario)							#	Compara la cadena introducida
+				print("		",BuscarPersonaje(Busqueda,ListaPersonajes(Diccionario)))	#	con la lista de todos los
+				print("		Apariciones >",Total,"\n")									#	personajes y si lo encuentra
+				Pausa()																	#	imprime el total de las apariciones
+																						
+			elif opcion==3:						################################
+				clear(1)						#	Personajes por categoría   #
+				Cat=Categorias(Diccionario)		################################
 				for categoria in Cat:
-					print("			",categoria)
+					print("			",categoria)	#	Imprime la lista de categorías y pide una por teclado
 				Busqueda=LimpiarCadena(input('''
 			 Introduce una categoria
 		  	 > '''))
-				for linea in PersonajesPorCategoria(Diccionario,Busqueda):
-						print(linea)
+				for linea in PersonajesPorCategoria(Diccionario,Busqueda):		# Imprime los personajes con esa
+						print(linea)											# categoría
 				Pausa()
 				del Cat
 			
-			elif opcion==4:
-				clear(1)
-				Events=ListaEventos(Diccionario)
+			elif opcion==4:							##############################
+				clear(1)							#	Menú de eventos marvel   #
+				Events=ListaEventos(Diccionario)	##############################
 				for evento in Events:
-					print("		",evento)
+					print("		",evento)			#	Lista los eventos y pide uno por teclado
 				Busqueda=LimpiarCadena(input('''
 			 Introduce un evento
 		  	 > '''))
 				Participantes=PersonajesPorEvento(Diccionario,Busqueda)
-				for participante in Participantes:
+				for participante in Participantes:		#	Lista los personajes que aparecen en ese evento
 					print("		",participante)
 				Pausa()
 
-			elif opcion==5:
-				Lista=ListaPersonajes(Diccionario)
-				for Personaje in Lista:
-					print("			",Personaje)
+			elif opcion==5:								#	Personaje, conocidos y enemigos
+				Lista=ListaPersonajes(Diccionario)		#######################################
+				for Personaje in Lista:					
+					print("			",Personaje)	#	Lista los personajes y pide uno por teclado
 				Busqueda=LimpiarCadena(input('''			Introduce un personaje
 		  	> '''))
-				Personaje=BuscarPersonaje(Busqueda,ListaPersonajes(Diccionario))
+				Personaje=BuscarPersonaje(Busqueda,ListaPersonajes(Diccionario))	#	Busca el personaje
 				
 				try:
 					clear(0)
-					print("	",Personaje,">>",Heroe_Villano(Personaje,Diccionario),"\n\n")
-					ConocidosYEventos=EventosP(Personaje,Diccionario)
-					for evento in ConocidosYEventos:
+					print("	",Personaje,">>",Heroe_Villano(Personaje,Diccionario),"\n\n")	#	Coloca la etiqueta
+					ConocidosYEventos=EventosP(Personaje,Diccionario)						#	Heroe/Villano/Reformado
+					for evento in ConocidosYEventos:			#	Recorre la lista de Conocidos y eventos y la imprime
 						
 						print("\n	En el evento '"+evento[0]+"' coincidió con",len(evento[1]),"personajes:")
 						for personajes in evento[1]:
 							print("	-",personajes,">>",Heroe_Villano(personajes,Diccionario))
 					
-					if Miniaturas:
+					if Miniaturas:		#	Si al principio del programa aceptaste las miniaturas o ya tenías el paquete
+										#	ejecuta el comando feh para abrir una ventana con una imagen del personaje
 						print("\n\n			Cargando imagen...")
 						system('feh -Za 125 --title "{}" {} &'.format(Personaje,Miniatura(Personaje,Diccionario)))
 					Pausa()
